cb6fcce47498ae3bae3eb193f77318d2e97f83b6
uhcats <stanislawplust@gmail.com>
2022-07-31 22:51:07 +0200

    make javascript weather

--- public/script.js
+++ public/script.js
@@ -1,28 +1,41 @@
-
+const temp = document.querySelector('.temperature');
+const day = document.querySelector('.day');
+const hour = document.querySelector('.hour');
+const city = document.querySelector('.city');
 
 function setTimeAndTemprature(data) {
   
-  const date = new Date();
-  const currentDate = {
-    month: date.getMonth(),
-    day: date.getDay(),
-    year: date.getFullYear(),
-    hour: date.getHours(),
-  }
-
-
-  console.log(currentDate.hour);
+  // const date = new Date();
+  // const currentDate = {
+  //   month: date.getMonth(),
+  //   day: date.getDay(),
+  //   year: date.getFullYear(),
+  //   hour: date.getHours(),
+  // }
+  // console.log(currentDate.hour);
   
   
   
 
   const {temperature,time} = data.current_weather;
 
+  const getDay = time.slice(0,10);
+  const getHour = time.slice(-5);
 
-  console.log(`Temperatura ${temperature} dnia ${time}`);
+  temp.textContent = temperature;
+  day.textContent = getDay;
+  hour.textContent = getHour;
+  city.textContent = 'Koszalin';
+
+  
+
+}
+const KoszalinGeography = {
+  latitude: 54.2,
+  longitude: 	16.1833,
 }
 
 
-fetch('https://api.open-meteo.com/v1/forecast?latitude=52.2297&longitude=21.0122&hourly=temperature_2m&current_weather=true')
+fetch(`https://api.open-meteo.com/v1/forecast?latitude=${KoszalinGeography.latitude}&longitude=${KoszalinGeography.longitude}&hourly=temperature_2m&current_weather=true`)
 .then(r => r.json())
-.then(data => console.log(setTimeAndTemprature(data)))
+.then(data => setTimeAndTemprature(data))
